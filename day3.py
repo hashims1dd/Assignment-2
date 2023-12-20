57d6932175c975b56b3e822f0df46875f107d7c2
hashims1dd <153985629+hashims1dd@users.noreply.github.com>
2023-12-20 16:16:50 +0000

    dictionary and sets

--- day3.py
+++ day3.py
@@ -11,7 +11,10 @@ True
 '''
 
 list1 = [1, 2, 4, 6, 8, 2, 1, 4, 10, 12, 14, 12, 16, 17]
-
+set2=set([1,22,22,4,3])
+dictionary={'hashim':21}
+print("original length",len(list1))
+print("set length",len(set(list1)))
 
 if len(set(list1)) == len(list1):
     print("They are unique")
