1d106d15fc1516ed51b8cabfd6edebbb93722266
hashims1dd <153985629+hashims1dd@users.noreply.github.com>
2023-12-22 14:15:48 +0000

    question1.py

---
+++ question1.py
@@ -0,0 +1,21 @@
+'''
+Write a Python program to insert a given string at the beginning of all items in a list.
+Sample list : [1,2,3,4], string : emp
+Expected output : ['emp1', 'emp2', 'emp3', 'emp4']
+'''
+
+list1 = [1,2,3,4]
+string = "emp"
+list2 = []
+
+
+def func(list1, string):
+    for i in range(0, len(list1)):
+        z = string + str(list1[i])
+        list2.append(z)
+
+    return list2
+    print(list2)
+
+
+print(func(list1, string))
